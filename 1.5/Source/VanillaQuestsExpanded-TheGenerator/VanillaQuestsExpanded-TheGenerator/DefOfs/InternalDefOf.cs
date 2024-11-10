c459c862eb45fda65a2335f3e23c9873280362da
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-11-10 15:40:48 +0100

    C# project skeleton

---
+++ 1.5/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/DefOfs/InternalDefOf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaQuestsExpandedTheGenerator
+{
+
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+       
+
+    }
+}
