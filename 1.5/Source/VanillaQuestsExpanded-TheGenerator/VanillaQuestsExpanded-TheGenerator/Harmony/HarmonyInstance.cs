70da1ddeeaef7e94eb3748f9d4ec1dec9b60db7c
Taranchuk <nfhfyxer@gmail.com>
2024-11-20 18:16:23 +0200

    InspectPaneFiller_DrawHealth_Patch

--- 1.5/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/Harmony/HarmonyInstance.cs
+++ 1.5/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/Harmony/HarmonyInstance.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using UnityEngine;
 using Verse;
-using RimWorld;
 using HarmonyLib;
 using System.Reflection;
 
 namespace VanillaQuestsExpandedTheGenerator
 {
 
-   
     [StaticConstructorOnStartup]
     public class Main
     {
