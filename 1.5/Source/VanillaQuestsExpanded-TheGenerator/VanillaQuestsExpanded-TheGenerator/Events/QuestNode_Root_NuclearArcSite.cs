b695305aa85d2741b1be730815967823c5bc7ce5
Taranchuk <nfhfyxer@gmail.com>
2025-01-06 20:29:54 +0200

    quest 7

--- 1.5/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/Events/QuestNode_Root_NuclearArcSite.cs
+++ 1.5/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/Events/QuestNode_Root_NuclearArcSite.cs
@@ -2,8 +2,8 @@ using RimWorld;
 
 namespace VanillaQuestsExpandedTheGenerator
 {
-    public class QuestNode_Root_NuclearArcSite : QuestNode_Root_ArcSite
-    {
-        public override SitePartDef QuestSite => InternalDefOf.VQE_Quest5Site_Nuclear;
-    }
+	public class QuestNode_Root_NuclearArcSite : QuestNode_Root_ArcSite
+	{
+		public override SitePartDef QuestSite => InternalDefOf.VQE_Quest5Site_Nuclear;
+	}
 }
