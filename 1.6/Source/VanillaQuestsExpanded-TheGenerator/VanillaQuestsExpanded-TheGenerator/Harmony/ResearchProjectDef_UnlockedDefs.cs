dcd827a8e9bbd548599609eb49f60975861a6452
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-07-05 08:14:39 +0200

    v2.032

---
+++ 1.6/Source/VanillaQuestsExpanded-TheGenerator/VanillaQuestsExpanded-TheGenerator/Harmony/ResearchProjectDef_UnlockedDefs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace VanillaQuestsExpandedTheGenerator
+{
+   
+    [HarmonyPatch(typeof(ResearchProjectDef))]
+    [HarmonyPatch("UnlockedDefs", MethodType.Getter)]
+    public static class VanillaQuestsExpandedTheGenerator_ResearchProjectDef_UnlockedDefs_Patch
+    {
+        public static void Postfix(ref ResearchProjectDef __instance, ref List<Def> __result)
+        {
+            if (__result.Contains(InternalDefOf.VQE_Genetron_Basic)) {
+                List<Def> newList = __result;
+                newList.Remove(InternalDefOf.VQE_Genetron_Basic);
+                __result = newList;
+            }
+
+        }
+    }
+}
